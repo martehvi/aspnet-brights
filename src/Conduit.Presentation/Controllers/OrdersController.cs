92861f9d5dd1f51b5499d12f1355d95efe85394c
Marte Haugestøyl Vinje <mhau@netlight.com>
2023-09-12 19:50:11 +0200

    Added start file for OrdersController

---
+++ src/Conduit.Presentation/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using Conduit.Domain.Entities;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conduit.Presentation.Controllers;
+
+[Route("[controller]")]
+[ApiExplorerSettings(GroupName = "Orders")]
+[Authorize]
+public class OrdersController
+{
+    public OrdersController()
+    {
+
+    }
+
+    /// <summary>
+    /// Get recent orders globally
+    /// </summary>
+    /// <remarks>Get all orders globally. </remarks>
+    /// <returns></returns>
+    [HttpGet(Name = "GetOrders")]
+    [AllowAnonymous]
+    public void List()
+    {
+        // insert GET logic here
+        // should return something similar as the GET endpoint in the other *Controller.cs files - like Task<MutlipleOrderResponse>
+    }
+}
